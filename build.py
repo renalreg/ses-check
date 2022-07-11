e81c7529781a47767b78620fe1f4af8f51871f23
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-07-11 08:11:07 +0000

    Revert "Revert to Excel format"
    This reverts commit fec2133d2e8aa3f106851113034d3165750d2632.

--- build.py
+++ build.py
@@ -1,28 +1,34 @@
-import io
+import csv
 import json
+from contextlib import closing
 import requests
-import pandas as pd
 
-url = "https://digital.nhs.uk/binaries/content/assets/website-assets/services/nhs-mail/secure-email-standard/dcb1596_accredited_domains.xlsx"
-
-r = requests.get(url)
-
-with io.BytesIO(r.content) as fh:
-    df = pd.io.excel.read_excel(fh, sheet_name=0)
+# Ok so super cool thing, NHS Digital have decided to keep the URL looking like an XLSX file, but it actually downloads a CSV file.
+# The HTTP response downloads the file with a filename "dcb1596_accredited_domains.csv".
+# Furthermore, it's full of non-UTF-8 characters, I assume because the file is being lazily converted from Excel to CSV
+URL = "https://digital.nhs.uk/binaries/content/assets/website-assets/services/nhs-mail/secure-email-standard/dcb1596_accredited_domains.xlsx"
 
 orgs = {}
 
-for row in df.itertuples():
-    d = {
-        "org": row[1],
-        "intl_proc": (row[4] == "Yes"),
-        "dmarc": row[5],
-        "spf": row[6],
-        "dkim": row[7] == "Y",
-        "tls12": row[8] == "Y",
-    }
+with closing(requests.get(URL, stream=True)) as r:
+    # NOTE: We need to add "ignore" to the decode to avoid exceptions where the file contains non-utf-8 characters
+    f = (line.decode("utf-8", "ignore") for line in r.iter_lines())
+    reader = csv.reader(f, delimiter=",", quotechar='"')
+    next(reader, None)  # Skip headers
+    for row in reader:
+        if not row:
+            continue
 
-    orgs[row[3].strip().lower()] = d
+        d = {
+            "org": row[0],
+            "intl_proc": (row[3] == "Yes"),
+            "dmarc": row[4],
+            "spf": row[5],
+            "dkim": row[6] == "Y",
+            "tls12": row[7] == "Y",
+        }
 
-with open("orgs.json", "w") as f:
+        orgs[row[2].strip().lower()] = d
+
+with open("orgs.json", "w", encoding="utf-8") as f:
     json.dump(orgs, f)
