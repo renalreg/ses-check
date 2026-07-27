60a70023df1f2b311f2960024025afadfb6f5e5a
Alice Morris <amorris66@hotmail.co.uk>
2026-07-27 14:17:44 +0100

    fix: raise an error on failed HTTP responses to prevent silent data loss

--- build.py
+++ build.py
@@ -8,6 +8,7 @@ URL = "https://digital.nhs.uk/binaries/content/assets/website-assets/services/nh
 orgs = {}
 
 with closing(requests.get(URL, stream=True)) as r:
+    r.raise_for_status()
     # NOTE: We need to add "ignore" to the decode to avoid exceptions where the file contains non-utf-8 characters
     f = (line.decode("utf-8", "ignore") for line in r.iter_lines())
     reader = csv.reader(f, delimiter=",", quotechar='"')
